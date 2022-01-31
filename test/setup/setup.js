1ee8332cff30d6b3a2be26dac415aa489917594b
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2022-01-31 08:10:04 -0500

    add test files
    Change-type: patch

---
+++ test/setup/setup.js
@@ -0,0 +1,3 @@
+module.exports.setup = () => {
+  console.log("I am setup! I will run before any test suite")
+}
---
+++ test/teardown/teardown.js
@@ -0,0 +1,3 @@
+module.exports.teardown = () => {
+  console.log("I am teardown! I will execute after all test suites have passed or failed!")
+}
